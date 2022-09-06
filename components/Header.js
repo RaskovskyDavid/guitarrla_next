55582742966d4d039ed2338caf6cd7450017c3e9
david <davidtucu@hotmail.com>
2022-09-06 17:36:44 -0300

    Header y css

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,16 @@
 import Link from "next/link"
+import Image from "next/image"
 import styles from '../styles/Header.module.css'
+
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className="contenedor">
-        <div>
+      <div className={styles.barra}>
+        <Link href="/">
 
-        </div>
-        <nav>
+            <Image width={400} height={100} src="/img/logo.svg" alt="Imagen Logo" />
+        </Link>
+        <nav className={styles.navegacion}>
             <Link href="/">Inicio</Link>
             <Link href="/nosotros">Nosotros</Link>
             <Link href="/blog">Blog</Link>
