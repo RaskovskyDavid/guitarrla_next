0d611609cc23c661fd32b68c9d24452a58a92f6d
david <davidtucu@hotmail.com>
2022-09-06 17:04:46 -0300

    Creando estilos con css

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import Link from "next/link"
+import styles from '../styles/Header.module.css'
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className="contenedor">
+        <div>
+
+        </div>
+        <nav>
+            <Link href="/">Inicio</Link>
+            <Link href="/nosotros">Nosotros</Link>
+            <Link href="/blog">Blog</Link>
+            <Link href="/rienda">Tienda</Link>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
